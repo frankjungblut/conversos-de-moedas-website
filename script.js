af3c2228744c4ef67a5fdc37d2e1e7c35a80bf66
Frank Jungblut <franksjungblut@gmail.com>
2024-03-16 10:02:04 -0300

    salvando

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const convertButton = document.querySelector(".converter-button")
-const selectedCurrency = document.querySelector(".currency-select-to-convert")
-const CurrentCurrency = document.querySelector(".currency-select-current")
+const CurrentCurrency = document.querySelector(".currency-select-current") //moeda local
+const selectedCurrency = document.querySelector(".currency-select-to-convert") // moeda estrangeira
+
 
 function convertValues() {
-    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const inputCurrencyValue = document.querySelector(".input-currency").value // valor digitado pelo usuario
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
     const currencyValueConverted = document.querySelector(".currency-value-converted")
+
     const dolarToday = 5
     const euroToday = 5.46
     const libraToday = 6.36
     const bitcoinToday = 340544.45
     const realToday = 0.20
+    
 
-    console.log(selectedCurrency)
+    console.log(inputCurrencyValue)
 
     //moeda local
     if (CurrentCurrency.value == "dolar") {
@@ -44,36 +47,64 @@ function convertValues() {
 
     //moeda convertida
     if (!(CurrentCurrency.value == selectedCurrency.value)) {
-        if (selectedCurrency.value == "dolar") {
-            const convertedValue = inputCurrencyValue / dolarToday
-            currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD"
-            }).format(convertedValue)
-        } else if (selectedCurrency.value == "euro") {
-            const convertedValue = inputCurrencyValue / euroToday
-            currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
-                style: "currency",
-                currency: "EUR"
-            }).format(convertedValue)
-        } else if (selectedCurrency.value == "libra") {
-            const convertedValue = inputCurrencyValue / libraToday
-            currencyValueConverted.innerHTML = new Intl.NumberFormat("en-UK", {
-                style: "currency",
-                currency: "GBP"
-            }).format(convertedValue)
-        } else if (selectedCurrency.value == "bitcoin") {
-            const convertedValue = inputCurrencyValue / bitcoinToday
-            currencyValueConverted.innerHTML = new Intl.NumberFormat(undefined, {
-                style: "currency",
-                currency: "BTC"
-            }).format(convertedValue)
-        } else if (selectedCurrency.value == "real") {
-            const convertedValue = inputCurrencyValue / realToday
-            currencyValueConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
-                style: "currency",
-                currency: "BRL"
-            }).format(convertedValue)
+        if (CurrentCurrency.value == "real") {
+            if (selectedCurrency.value == "dolar") {
+                const convertedValue = inputCurrencyValue / dolarToday
+                currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "USD"
+                }).format(convertedValue)
+            } else if (selectedCurrency.value == "euro") {
+                const convertedValue = inputCurrencyValue / euroToday
+                currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+                    style: "currency",
+                    currency: "EUR"
+                }).format(convertedValue)
+            } else if (selectedCurrency.value == "libra") {
+                const convertedValue = inputCurrencyValue / libraToday
+                currencyValueConverted.innerHTML = new Intl.NumberFormat("en-UK", {
+                    style: "currency",
+                    currency: "GBP"
+                }).format(convertedValue)
+            } else if (selectedCurrency.value == "bitcoin") {
+                const convertedValue = inputCurrencyValue / bitcoinToday
+                currencyValueConverted.innerHTML = new Intl.NumberFormat(undefined, {
+                    style: "currency",
+                    currency: "BTC"
+                }).format(convertedValue)
+            } else if (selectedCurrency.value == "real") {
+                const convertedValue = inputCurrencyValue / realToday
+                currencyValueConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
+                    style: "currency",
+                    currency: "BRL"
+                }).format(convertedValue)
+            }
+        }else if(CurrentCurrency.value == "dolar"){//moeda local dolar
+            if (selectedCurrency.value == "euro") {
+                const convertedValue = inputCurrencyValue / euroToday
+                currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+                    style: "currency",
+                    currency: "EUR"
+                }).format(convertedValue)
+            } else if (selectedCurrency.value == "libra") {
+                const convertedValue = inputCurrencyValue / libraToday
+                currencyValueConverted.innerHTML = new Intl.NumberFormat("en-UK", {
+                    style: "currency",
+                    currency: "GBP"
+                }).format(convertedValue)
+            } else if (selectedCurrency.value == "bitcoin") {
+                const convertedValue = inputCurrencyValue / bitcoinToday
+                currencyValueConverted.innerHTML = new Intl.NumberFormat(undefined, {
+                    style: "currency",
+                    currency: "BTC"
+                }).format(convertedValue)
+            } else if (selectedCurrency.value == "real") {
+                const convertedValue = inputCurrencyValue / realToday
+                currencyValueConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
+                    style: "currency",
+                    currency: "BRL"
+                }).format(convertedValue)
+            }
         }
     }
     //par igual de moeda
